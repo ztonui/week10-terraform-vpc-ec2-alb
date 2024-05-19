82301210197dfe736539206039ea327581ad1980
zeddy <zeddytonui@example.com>
2024-05-18 23:24:14 -0400

    jenkinsfile3

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "state-log"
+    dynamodb_table = "Terraform-jenk"
   }
 }
